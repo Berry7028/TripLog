79a4f0aab3ca35396c778b93e6e7b8a1fe7bdb39
Berry <s.kouta7028@gmail.com>
2025-09-08 16:42:30 +0900

    Load core settings from environment

--- travel_log_map/settings.py
+++ travel_log_map/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=c9_%nizo8@fm9njwyx)f*_^qd(jv^1@(!613de^98a93*d3=h'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-=c9_%nizo8@fm9njwyx)f*_^qd(jv^1@(!613de^98a93*d3=h",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() in {"true", "1", "yes"}
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in os.environ.get(
+        "ALLOWED_HOSTS", "localhost,127.0.0.1,testserver"
+    ).split(",")
+]
 
 
 # Application definition
